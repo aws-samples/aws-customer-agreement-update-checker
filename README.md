51c277f7e02226a1894fc82f61410588346e02c6
Chris Sciarrino <csciar@amazon.com>
2024-02-21 18:36:29 -0500

    Push approved release

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-## My Project
+## AWS Customer Agreement Update Checker
 
-TODO: Fill this README out!
+This repository contains a simple solution to check and notify when it detects a change to the AWS customer agreement posted at https://aws.amazon.com/agreement/
 
-Be sure to:
 
-* Change the title in this README
-* Edit your repository description on GitHub
+The solution uses an Amazon EventBridge rule which triggers a Lambda function daily. This function stores the "Last Updated" date specified on the agreement page in an SSM parameter and compares the date found with the stored parameter. When a new date is detected, an SNS notification is sent to the email addresses subscribed to the topic.
 
-## Security
+## Deployment
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+### Clone the repository
+`git@github.com:aws-samples/aws-customer-agreement-update-checker.git`
+
+### Navigate to the cloned repository
+`cd aws-customer-agreement-update-checker`
+
+### Run the cloudformation package command
+`aws cloudformation package --template-file cloudformation.yml --s3-bucket <bucket_name> --output-template-file packaged.yaml`
+Replace the <bucket_name> with an existing bucket name in your account
+
+### Deploy the CloudFormation stack
+`aws cloudformation deploy --template-file packaged.yaml --stack-name aws-ca-update-checker-stack --parameter-overrides "NotificationEmailAddress=pat_candella@example.org" --capabilities CAPABILITY_IAM`
 
 ## License
 
--- ca_update_check/ca_update_check.py
+++ ca_update_check/ca_update_check.py
@@ -1,6 +1,6 @@
 #This lambda function will download an HTML page and parse the page looking for the string "Last Updated" and date in the string
-#It will check against a DynamoDB table to see if the date is the same as the last time the function was run
-#If the date is different, it will update the DynamoDB table with the new date and send an SNS notification
+#It will check against an SSM parameter store to see if the date is the same as the last time the function was run
+#If the date is different, it will update the SSM parameter store with the new date and send an SNS notification
 #If the date is the same, it will do nothing and return a success message
 #The SNS notification will be sent to a SNS topic that is subscribed to the lambda function
 #The SNS topic will be used to send an email to the user with the new date
@@ -34,18 +34,17 @@ def lambda_handler(event, context):
     if match:
         last_updated_date = match.group(1)
 
-        # Connect to DynamoDB
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(os.environ.get('DYNAMODB_TABLE_NAME'))
-        response = table.get_item(Key={'id': '1'})
-        # Check if the date in the DynamoDB table matches the date in the HTML content
-        if 'Item' in response and response['Item']['date'] == last_updated_date:
-            print("Date in DynamoDB table matches the date in the HTML content")
+        # Connect to SSM parameter
+        ssm = boto3.client('ssm')
+        response = ssm.get_parameter(Name='/customer_agreement_last_updated_date')
+        last_updated_date_from_ssm = response['Parameter']['Value']
+        
+        if last_updated_date_from_ssm == last_updated_date:
+            print("Date in SSM parameter matches the date in the HTML content")
 
-        # If the date in the DynamoDB table does not match the date in the HTML content, update the DynamoDB table and send an SNS notification
         else:
-            print("Date in DynamoDB table does not match the date in the HTML content")
-            table.put_item(Item={'id':'1','date': last_updated_date})
+            print("Date in SSM does not match the date in the HTML content")
+            ssm.put_parameter(Name='/customer_agreement_last_updated_date', Value=last_updated_date, Type='String',Overwrite=True)
 
             # Connect to SNS
             sns = boto3.client('sns')
